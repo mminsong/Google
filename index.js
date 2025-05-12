f417afd3dd7e8428a7329a42fc8580ebe4288883
mminsong <pamten@naver.com>
2025-05-12 22:29:13 +0900

    데이터터

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+//팝업 띄우기
 function popUp() {
-  document.getElement('popup').style.visibility='visible';
+  document.getElementById('popup').style.visibility = 'visible';
+}
+
+//취소 누르면 팝업 닫기
+function cancel() {
+  document.getElementById('popup').style.visibility = 'hidden';
+}
+
+//저장 --> 이름, url 입력 후 input 태그 초기화
+function save() {
+  var name = document.getElementById('name');
+  var url = document.getElementById('url');
+  document.getElementById('popup').style.visibility = 'hidden';
+  name.value = null;
+  url.value = null;
 }
